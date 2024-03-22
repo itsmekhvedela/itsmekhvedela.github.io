60b386bce67960f03d83ece989abab59cff5b430
KhvedelaSenpai <khvedelidze.nik@gmail.com>
2024-03-22 22:59:46 +0400

    Create youtube.php

---
+++ links/youtube.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Khvedela, Khvedelasenpai, Nikoloz Khvedelidze">
+        <meta name="description" content="Khvedela's main website">
+        <meta name="author" content="Nicholas Khvedelidze">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../stylesv2-0.css">
+        <title>khvedela</title>
+        <link rel="icon" href="../icons/logo.png">
+        <script src="../myScript.js"></script>
+    </head>
+    <body id="body" onload="loadTheme()">
+        <header>
+            <br><br>
+        </header>
+        <nav id="navbar">
+            <a class="nav-button" href="../index.php">Home</a>
+            <a class="nav-button" href="../about-me.php">About Me</a>
+            <a class="nav-button" href="#">Contact Me</a>
+            <button class="nav-button-open" id="navButtonOpen" onclick="openSideNav()">Ξ</button>
+        </nav>
+        <div id="mySidenav" class="sidenav">
+            <a class="nav-button" href="../upcoming-updates.html" title="Upcoming Updates">
+                Upcoming Updates
+            </a>
+            <a class="nav-button" href="fatex.php" title="FateX discord community">
+                FateX Discord
+            </a>
+            <a class="nav-button" href="discord.php" title="My Discord group">
+                My Discord
+            </a>
+            <a class="nav-button-active" href="youtube.php" title="My Youtube channel">
+                My Youtube
+            </a>
+            <button class="btn-toggle" onclick="toggleMode()">◐</button>
+        </div>
+        <main>
+            <h1>My Youtube channel</h1>
+            <div class="div-container">
+                <div class="div-box">
+                    <span class="box-text">Information will be available soon!</span>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <p>&copy;Nicholas Khvedelidze. All rights reserved</p>
+        </footer>
+    </body>
+</html>
