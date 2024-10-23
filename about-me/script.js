1234ef782c42a15cc42831a790a799210ca959f8
KhvedelaSenpai <khvedelidze.nik@gmail.com>
2024-10-23 16:41:08 +0400

    Delete script.js

--- about-me/script.js
+++
@@ -1,9 +0,0 @@
-function showInfo(){
-    if (document.getElementById("additionalInfo").style.display === "flex"){
-        document.getElementById("additionalInfo").style.display = "none";
-        document.getElementById("showInfoBtn").textContent = "Show more";
-    } else {
-        document.getElementById("additionalInfo").style.display = "flex";
-        document.getElementById("showInfoBtn").textContent = "Hide all";
-    }
-}
